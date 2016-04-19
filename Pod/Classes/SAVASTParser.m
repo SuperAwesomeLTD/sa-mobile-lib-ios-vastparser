fac75f84a66da989334cdb683de42f3c559cd298
Gabriel Coman <dev.gabriel.coman@gmail.com>
2016-04-19 16:22:17 +0100

    Use SAVastParser instead of TBXML (1.0.7)

--- Pod/Classes/SAVASTParser.m
+++ Pod/Classes/SAVASTParser.m
@@ -8,9 +8,8 @@
 
 #import "SAVASTParser.h"
 
-// import XML
-#import "TBXML.h"
-#import "TBXML+SAStaticFunctions.h"
+// import new SAXML
+#import "SAXMLParser.h"
 
 // import helpes
 #import "SAVASTAd.h"
@@ -58,26 +57,27 @@
     
     // step 1: get the XML
     NSData *xmlData = [SAUtils sendSyncGETToEndpoint:vastURL];
-    NSError *xmlError = NULL;
-    TBXML *tbxml = [[TBXML alloc] initWithXMLData:xmlData error:&xmlError];
-    if (xmlError) {
+    SAXMLParser *parser = [[SAXMLParser alloc] init];
+    __block SAXMLElement *root = [parser parseXMLData:xmlData];
+    if ([parser getError]) {
+        NSLog(@"%@", [parser getError]);
         return ads;
     }
     
-    // step 2. get the correct reference to the root XML element
-    __block TBXMLElement *root = tbxml.rootXMLElement;
+    // print levels - just for show
+    [parser printLevels];
     
     // step 3. if no "Ad" elements are found, just don't continue
-    if (![TBXML checkSiblingsAndChildrenOf:root forName:@"Ad"]){
+    if (![SAXMLParser checkSiblingsAndChildrenOf:root forName:@"Ad"]){
         return ads;
     }
     
     // step 4. start finding ads and parsing them
-    [TBXML searchSiblingsAndChildrenOf:root forName:@"Ad" andInterate:^(TBXMLElement *adElement) {
+    [SAXMLParser searchSiblingsAndChildrenOf:root forName:@"Ad" andInterate:^(SAXMLElement *adElement) {
         
         // check ad type
-        BOOL isInLine = [TBXML checkSiblingsAndChildrenOf:adElement->firstChild forName:@"InLine"];
-        BOOL isWrapper = [TBXML checkSiblingsAndChildrenOf:adElement->firstChild forName:@"Wrapper"];
+        BOOL isInLine = [SAXMLParser checkSiblingsAndChildrenOf:adElement forName:@"InLine"];
+        BOOL isWrapper = [SAXMLParser checkSiblingsAndChildrenOf:adElement forName:@"Wrapper"];
         
         // normal InLine case
         if (isInLine) {
@@ -91,10 +91,9 @@
             
             // get VAStAdTagURI
             NSString *VASTAdTagURI = @"";
-            NSValue *uriPointer = [TBXML findFirstIntanceInSiblingsAndChildrenOf:adElement->firstChild forName:@"VASTAdTagURI"];
-            if (uriPointer) {
-                TBXMLElement *uriElement = [uriPointer pointerValue];
-                VASTAdTagURI = [SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:uriElement]];
+            SAXMLElement *redirect = [SAXMLParser findFirstIntanceInSiblingsAndChildrenOf:adElement forName:@"VASTAdTagURI"];
+            if (redirect) {
+                VASTAdTagURI = [redirect getValue];
             }
             
             // call back this function recursevly
@@ -118,13 +117,13 @@
     return ads;
 }
 
-- (SAVASTAd*) parseAdXML:(TBXMLElement *)adElement {
+- (SAVASTAd*) parseAdXML:(SAXMLElement *)adElement {
     // create ad
     SAVASTAd *ad = [[SAVASTAd alloc] init];
     
     // get attributes
-    ad._id = [TBXML valueOfAttributeNamed:@"id" forElement:adElement];
-    ad.sequence = [TBXML valueOfAttributeNamed:@"sequence" forElement:adElement];
+    ad._id = [adElement getAttribute:@"id"];
+    ad.sequence = [adElement getAttribute:@"sequence"];
     ad.type = Invalid;
     
     // init arrays of data
@@ -133,28 +132,28 @@
     ad.Creatives = [[NSMutableArray alloc] init];
     
     // check ad type
-    BOOL isInLine = [TBXML checkSiblingsAndChildrenOf:adElement->firstChild forName:@"InLine"];
-    BOOL isWrapper = [TBXML checkSiblingsAndChildrenOf:adElement->firstChild forName:@"Wrapper"];
+    BOOL isInLine = [SAXMLParser checkSiblingsAndChildrenOf:adElement forName:@"InLine"];
+    BOOL isWrapper = [SAXMLParser checkSiblingsAndChildrenOf:adElement forName:@"Wrapper"];
     
     if (isInLine) ad.type = InLine;
     if (isWrapper) ad.type = Wrapper;
     
     // get errors
-    [TBXML searchSiblingsAndChildrenOf:adElement->firstChild forName:@"Error" andInterate:^(TBXMLElement *errElement) {
-        [ad.Errors addObject:[SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:errElement]]];
+    [SAXMLParser searchSiblingsAndChildrenOf:adElement forName:@"Error" andInterate:^(SAXMLElement *errElement) {
+        [ad.Errors addObject:[SAUtils decodeHTMLEntitiesFrom:[errElement getValue]]];
     }];
     
     // get impressions
-    [TBXML searchSiblingsAndChildrenOf:adElement->firstChild forName:@"Impression" andInterate:^(TBXMLElement *impElement) {
+    [SAXMLParser searchSiblingsAndChildrenOf:adElement forName:@"Impression" andInterate:^(SAXMLElement *impElement) {
         // the impression object is now a more complex one
         SAImpression *impr = [[SAImpression alloc] init];
         impr.isSent = false;
-        impr.URL = [SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:impElement]];
+        impr.URL = [SAUtils decodeHTMLEntitiesFrom:[impElement value]];
         [ad.Impressions addObject:impr];
     }];
     
     // get creatives
-    [TBXML searchSiblingsAndChildrenOf:adElement->firstChild forName:@"Creative" andInterate:^(TBXMLElement *creativeElement) {
+    [SAXMLParser searchSiblingsAndChildrenOf:adElement forName:@"Creative" andInterate:^(SAXMLElement *creativeElement) {
         SALinearCreative *linear = [self parseCreativeXML:creativeElement];
         if (linear) {
             [ad.Creatives addObject:linear];
@@ -166,10 +165,10 @@
     return ad;
 }
 
-- (SALinearCreative*) parseCreativeXML:(TBXMLElement *)element {
+- (SALinearCreative*) parseCreativeXML:(SAXMLElement *)element {
     // first find out what kind of content this creative has
     // is it Linear, NonLinear or CompanionAds?
-    BOOL isLinear = [TBXML checkSiblingsAndChildrenOf:element->firstChild forName:@"Linear"];
+    BOOL isLinear = [SAXMLParser checkSiblingsAndChildrenOf:element forName:@"Linear"];
     
     // init as a linear Creative
     if (isLinear) {
@@ -178,8 +177,8 @@
         
         // get attributes
         _creative.type = Linear;
-        _creative._id = [TBXML valueOfAttributeNamed:@"id" forElement:element];
-        _creative.sequence = [TBXML valueOfAttributeNamed:@"sequence" forElement:element];
+        _creative._id = [element getAttribute:@"id"];
+        _creative.sequence = [element getAttribute:@"sequence"];
         
         // create arrays
         _creative.ClickTracking = [[NSMutableArray alloc] init];
@@ -188,13 +187,13 @@
         _creative.TrackingEvents = [[NSMutableArray alloc] init];
         
         // populate duration
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"Duration" andInterate:^(TBXMLElement *durElement) {
-            _creative.Duration = [TBXML textForElement:durElement];
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"Duration" andInterate:^(SAXMLElement *durElement) {
+            _creative.Duration = [durElement value];;
         }];
         
         // populate clickthrough
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"ClickThrough" andInterate:^(TBXMLElement *clickElement) {
-            _creative.ClickThrough = [TBXML textForElement:clickElement];
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"ClickThrough" andInterate:^(SAXMLElement *clickElement) {
+            _creative.ClickThrough = [clickElement value];;
             _creative.ClickThrough = [SAUtils decodeHTMLEntitiesFrom:_creative.ClickThrough];
             _creative.ClickThrough = [_creative.ClickThrough stringByReplacingOccurrencesOfString:@"&amp;" withString:@"&"];
             _creative.ClickThrough = [_creative.ClickThrough stringByReplacingOccurrencesOfString:@"%3A" withString:@":"];
@@ -202,25 +201,25 @@
         }];
         
         // populate click tracking array
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"ClickTracking" andInterate:^(TBXMLElement *ctrackElement) {
-            [_creative.ClickTracking addObject:[SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:ctrackElement]]];
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"ClickTracking" andInterate:^(SAXMLElement *ctrackElement) {
+            [_creative.ClickTracking addObject:[SAUtils decodeHTMLEntitiesFrom:[ctrackElement value]]];
         }];
         
         // populate custom clicks array
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"CustomClicks" andInterate:^(TBXMLElement *cclickElement) {
-            [_creative.CustomClicks addObject:[SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:cclickElement]]];
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"CustomClicks" andInterate:^(SAXMLElement *cclickElement) {
+            [_creative.CustomClicks addObject:[SAUtils decodeHTMLEntitiesFrom:[cclickElement value]]];
         }];
         
         // populate media files
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"MediaFile" andInterate:^(TBXMLElement *cMediaElement) {
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"MediaFile" andInterate:^(SAXMLElement *cMediaElement) {
             // since this is a more "complex" object, wich takes data from
             // attributes as well as tag value, split the
             // declaration and array assignment
             SAMediaFile *mediaFile = [[SAMediaFile alloc] init];
-            mediaFile.width = [TBXML valueOfAttributeNamed:@"width" forElement:cMediaElement];
-            mediaFile.height = [TBXML valueOfAttributeNamed:@"height" forElement:cMediaElement];
-            mediaFile.type = [TBXML valueOfAttributeNamed:@"type" forElement:cMediaElement];
-            mediaFile.URL = [SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:cMediaElement]];
+            mediaFile.width = [cMediaElement getAttribute:@"width"];
+            mediaFile.height = [cMediaElement getAttribute:@"height"];
+            mediaFile.type = [cMediaElement getAttribute:@"type"];
+            mediaFile.URL = [SAUtils decodeHTMLEntitiesFrom:[cMediaElement value]];
             
             // only add the Media file if the type is MP4
             if ([mediaFile.type rangeOfString:@"mp4"].location != NSNotFound ||
@@ -230,13 +229,13 @@
         }];
         
         // populate tracking
-        [TBXML searchSiblingsAndChildrenOf:element->firstChild forName:@"Tracking" andInterate:^(TBXMLElement *cTrackingElement) {
+        [SAXMLParser searchSiblingsAndChildrenOf:element forName:@"Tracking" andInterate:^(SAXMLElement *cTrackingElement) {
             // since this is also a more "complex" object, which takes data from
             // attributes as well as tag value, split the declaration and
             // array assignmenent
             SATracking *tracking = [[SATracking alloc] init];
-            tracking.event = [TBXML valueOfAttributeNamed:@"event" forElement:cTrackingElement];
-            tracking.URL = [SAUtils decodeHTMLEntitiesFrom:[TBXML textForElement:cTrackingElement]];
+            tracking.event = [cTrackingElement getAttribute:@"event"];
+            tracking.URL = [SAUtils decodeHTMLEntitiesFrom:[cTrackingElement value]];
             [_creative.TrackingEvents addObject:tracking];
         }];
         
