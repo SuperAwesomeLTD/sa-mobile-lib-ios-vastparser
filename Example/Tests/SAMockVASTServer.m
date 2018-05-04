792597d0f1af8c8464f7056b18a446dcf198c2c5
Gabriel Coman <dev.gabriel.coman@gmail.com>
2018-05-04 12:50:45 +0100

    Added a response factory

--- Example/Tests/SAMockVASTServer.m
+++ Example/Tests/SAMockVASTServer.m
@@ -8,17 +8,17 @@
 
 #import "SAMockVASTServer.h"
 #import <OHHTTPStubs/OHHTTPStubs.h>
-#import "SATestUtils.h"
+#import "SAResponseFactory.h"
 
 @interface SAMockVASTServer ()
-@property (nonatomic, strong) SATestUtils *utils;
+@property (nonatomic, strong) SAResponseFactory *factory;
 @end
 
 @implementation SAMockVASTServer
 
 - (id) init {
     if (self = [super init]) {
-        _utils = [[SATestUtils alloc] init];
+        _factory = [[SAResponseFactory alloc] init];
     }
     return self;
 }
@@ -28,34 +28,29 @@
         return true;
     } withStubResponse:^OHHTTPStubsResponse * _Nonnull(NSURLRequest * _Nonnull request) {
         NSString *url = [request URL].absoluteString;
-        if ([url containsString:@"/vast/vast1.xml"])        return [self sendResponse:@"mock_vast_response_1.0"];
-        else if ([url containsString:@"/vast/vast2.0.xml"]) return [self sendResponse:@"mock_vast_response_2.0"];
-        else if ([url containsString:@"/vast/vast2.1.xml"]) return [self sendResponse:@"mock_vast_response_2.1"];
-        else if ([url containsString:@"/vast/vast3.0.xml"]) return [self sendResponse:@"mock_vast_response_3.0"];
-        else if ([url containsString:@"/vast/vast3.1.xml"]) return [self sendResponse:@"mock_vast_response_3.1"];
-        else if ([url containsString:@"/vast/vast4.0.xml"]) return [self sendResponse:@"mock_vast_response_4.0"];
-        else if ([url containsString:@"/vast/vast5.0.xml"]) return [self sendResponse:@"mock_vast_response_5.0"];
-        else if ([url containsString:@"/vast/vast5.1.xml"]) return [self sendResponse:@"mock_vast_response_5.1"];
-        else if ([url containsString:@"/vast/vast5.2.xml"]) return [self sendResponse:@"mock_vast_response_5.2"];
-        else if ([url containsString:@"/vast/vast5.3.xml"]) return [self sendResponse:@"mock_vast_response_5.3"];
-        else return [self sendError];
+        if ([url containsString:@"/vast/vast1.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_1.0"];
+        else if ([url containsString:@"/vast/vast2.0.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_2.0"];
+        else if ([url containsString:@"/vast/vast2.1.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_2.1"];
+        else if ([url containsString:@"/vast/vast3.0.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_3.0"];
+        else if ([url containsString:@"/vast/vast3.1.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_3.1"];
+        else if ([url containsString:@"/vast/vast4.0.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_4.0"];
+        else if ([url containsString:@"/vast/vast5.0.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_5.0"];
+        else if ([url containsString:@"/vast/vast5.1.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_5.1"];
+        else if ([url containsString:@"/vast/vast5.2.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_5.2"];
+        else if ([url containsString:@"/vast/vast5.3.xml"])
+            return [self->_factory sendResponse:@"mock_vast_response_5.3"];
+        else
+            return [self->_factory sendError];
     }];
-}
-
-- (NSString*) sendVASTTag: (NSString*) xml {
-    return [_utils fixtureWithName:xml ofType:@"xml"];
-}
-
-- (OHHTTPStubsResponse*) sendResponse: (NSString*) xml {
-    NSString *xmlString = [self sendVASTTag:xml];
-    NSData *data = [xmlString dataUsingEncoding:NSUTF8StringEncoding];
-    NSDictionary *headers = @{@"Content-Type": @"application/xml"};
-    return [OHHTTPStubsResponse responseWithData:data statusCode:200 headers:headers];
-}
-
-- (OHHTTPStubsResponse*) sendError {
-    NSError* notConnectedError = [NSError errorWithDomain:NSURLErrorDomain code:kCFURLErrorNotConnectedToInternet userInfo:nil];
-    return [OHHTTPStubsResponse responseWithError:notConnectedError];
 }
 
 - (void) shutdown {
